0f6252e5726ee7efcb48babfc06400ff1b0b58ec
lhervi@gmail.com <lhervi@gmail.com>
2021-01-28 14:06:09 -0400

    All in typescript

--- React_Project/logExample/src/index.js
+++ React_Project/logExample/src/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-dom'
+import store from './store'
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const rootElement = document.getElementById('root')
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
+  rootElement
 );
 
 // If you want your app to work offline and load faster, you can change
